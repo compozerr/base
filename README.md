fc3ebf183d46f5a6d51c7b1c16af2237eefd6266
Milo <tomilobas@gmail.com>
2024-11-03 19:22:56 +0100

    Install dependencies

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # base
 
+# Prerequisites
+- Node.js
+- .NET
+
+# Install dependencies
+
+```bash
+npm run install
+```
+
+# Run dev server
+
+```bash
+npm run dev
+```
+
 # Deploy
 
 ```bash
---
+++ scripts/install.ts
@@ -0,0 +1,43 @@
+import { exec } from "child_process";
+
+console.log("Installing dependencies...");
+
+let denoInstalled = true;
+
+//Check if deno is installed
+exec("deno --version", (error, stdout, stderr) => {
+    if (error) {
+        denoInstalled = false;
+        return;
+    }
+});
+
+if (!denoInstalled) {
+    //if windows
+    if (process.platform === "win32") {
+        console.log("Deno is not installed, installing...");
+        exec("iwr https://deno.land/x/install/install.ps1 -useb | iex");
+    } else if (process.platform === "linux" || process.platform === "darwin") {
+        console.log("Deno is not installed, installing...");
+        exec("curl -fsSL https://deno.land/x/install/install.sh | sh");
+    } else {
+        console.error("Unsupported platform");
+    }
+}
+
+let nbgvInstalled = true;
+
+//Check if nbgv is installed
+exec("dotnet tool list -g | findstr nbgv", (error, stdout, stderr) => {
+    if (error) {
+        nbgvInstalled = false;
+        return;
+    }
+});
+
+if (!nbgvInstalled) {
+    console.log("nbgv is not installed, installing...");
+    exec("dotnet tool install nbgv");
+}
+
+console.log("Dependencies installed!");
