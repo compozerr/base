7502aef086565f8a38fcfbe02c4d9df2e5cb50d0
Milo <tomilobas@gmail.com>
2024-11-05 20:43:59 +0100

    Only show devtools in development

--- src/frontend/src/routes/__root.tsx
+++ src/frontend/src/routes/__root.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react'
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : React.lazy(() =>
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    )
 
 function RootComponent() {
   return (
