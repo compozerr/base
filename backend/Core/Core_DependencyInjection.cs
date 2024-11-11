ead10d9dad757c18a4881f482a0eecd019744065
Milo <tomilobas@gmail.com>
2024-11-11 21:42:44 +0100

    Add web api config

--- backend/Core/Core_DependencyInjection.cs
+++ backend/Core/Core_DependencyInjection.cs
@@ -1,12 +1,27 @@
+using Base.Helpers;
+
 namespace Base.Core;
 
 public static class Core_DependencyInjection
 {
     public static IServiceCollection AddCore(this IServiceCollection services)
-    => services.AddEndpointsApiExplorer()
-               .AddSwaggerGen()
-               .AddCors(new Action<Microsoft.AspNetCore.Cors.Infrastructure.CorsOptions>(options =>
-                {
-                    options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-                }));
+    => services.AddWebApiConfig();
+
+    public static IServiceCollection AddWebApiConfig(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: AppConstants.CorsPolicy,
+                builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new() { Title = "conpozerr base", Version = "v1" });
+        });
+
+
+        return services;
+    }
 }
---
+++ backend/Helpers/AppConfig.cs
@@ -0,0 +1,6 @@
+namespace Base.Helpers;
+
+public class AppConstants
+{
+    public const string CorsPolicy = nameof(CorsPolicy);
+}
