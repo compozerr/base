c70b6a889a4c7f7bcd61da555c299a3f23d43e3f
Milo <tomilobas@gmail.com>
2024-11-11 21:34:08 +0100

    Allow cors all origins if development

--- backend/Core/Core_DependencyInjection.cs
+++ backend/Core/Core_DependencyInjection.cs
@@ -4,5 +4,9 @@ public static class Core_DependencyInjection
 {
     public static IServiceCollection AddCore(this IServiceCollection services)
     => services.AddEndpointsApiExplorer()
-               .AddSwaggerGen();
+               .AddSwaggerGen()
+               .AddCors(new Action<Microsoft.AspNetCore.Cors.Infrastructure.CorsOptions>(options =>
+                {
+                    options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                }));
 }
