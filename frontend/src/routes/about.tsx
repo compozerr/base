c7c8d42c60fe210f6c389155178d7eab3b718453
Milo <tomilobas@gmail.com>
2024-11-15 20:50:59 +0100

    Using example api instead

--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
-import { ApiApi } from '../generated';
+import { ExampleApi } from '../generated';
 
 export const Route = createFileRoute('/about')({
   component: AboutComponent,
 })
 
 function AboutComponent() {
-  ApiApi.getExample({
+  ExampleApi.getExample({
     queries: {
       name: 'World!'
     }
