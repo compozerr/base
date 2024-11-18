1ea1a2d6a5b2bd7785e8752002c7fa7f404ea74d
Milo <tomilobas@gmail.com>
2024-11-18 16:47:19 +0100

    Use port 5000

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -1,6 +1,12 @@
+using System.Net;
 using Core.Feature;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000);
+});
 
 builder.Services.AddFeatures();
 
