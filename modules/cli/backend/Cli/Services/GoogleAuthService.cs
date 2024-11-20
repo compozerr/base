7d8e306ebbc085b2b3191d1159b910a3a72b4ef2
Milo <tomilobas@gmail.com>
2024-11-20 17:46:40 +0100

    Using parsed json

--- modules/cli/backend/Cli/Services/GoogleAuthService.cs
+++ modules/cli/backend/Cli/Services/GoogleAuthService.cs
@@ -1,20 +1,26 @@
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Cli.Services;
 
-public class GoogleAuthService
+public class GoogleAuthService(IConfiguration configuration)
 {
-    private readonly IConfiguration _configuration;
-
-    public GoogleAuthService(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
     public void SetupGoogleCredentials()
     {
-        var serviceAccountJson = _configuration["GOOGLECLOUD_SERVICEACCOUNTCREDENTIALS"];
-        File.WriteAllText("/tmp/google-credentials.json", serviceAccountJson);
-        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/tmp/google-credentials.json");
+        const string GOOGLE_APPLICATION_CREDENTIALS_PATH = "/tmp/google-credentials.json";
+        
+        var serviceAccountRaw = configuration["GOOGLECLOUD_SERVICEACCOUNTCREDENTIALS"];
+        if (string.IsNullOrEmpty(serviceAccountRaw))
+        {
+            Log.Logger.Error("GOOGLECLOUD_SERVICEACCOUNTCREDENTIALS is not set");
+            return;
+        }
+
+        var serviceAccountJson = JsonDocument.Parse(serviceAccountRaw);
+
+        File.WriteAllText(GOOGLE_APPLICATION_CREDENTIALS_PATH, serviceAccountJson.RootElement.ToString());
+
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", GOOGLE_APPLICATION_CREDENTIALS_PATH);
     }
 }
