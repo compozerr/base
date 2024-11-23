3c84b606f25ddc9b6b5e57e795f702a2f2d7b6fe
Milo <tomilobas@gmail.com>
2024-11-23 16:27:08 +0100

    Push to fly io registry

--- modules/cli/backend/Cli/Features/Hosting/Providers/Flyio/FlyioHostingProvider.cs
+++ modules/cli/backend/Cli/Features/Hosting/Providers/Flyio/FlyioHostingProvider.cs
@@ -14,7 +14,7 @@ public class FlyioHostingProvider(
 
     public async Task<DeployResponse> DeployAsync(DeployRequest deployRequest)
     {
-        var flyioAccessToken = $"'{configuration["FLYIO_ACCESS_TOKEN"]}'";
+        var flyioAccessToken = $"\"{configuration["FLYIO_ACCESS_TOKEN"]}\"";
 
         if (string.IsNullOrEmpty(flyioAccessToken))
         {
@@ -24,31 +24,96 @@ public class FlyioHostingProvider(
 
         Log.ForContext(nameof(deployRequest), deployRequest).Information("Deploying to Fly.io");
 
+        if (!await AuthenticateFlyioWithDockerAsync())
+        {
+            return new DeployResponse(false, "Failed to authenticate Fly.io with Docker");
+        }
+
+        var newTag = await TagAndPushImageToFlyioAsync(deployRequest.AppName, deployRequest.RegistryPath);
+
+        if (string.IsNullOrEmpty(newTag))
+        {
+            return new DeployResponse(false, "Failed to tag image to Fly.io");
+        }
+
         var hasBeenDeployed = await HasBeenDeployedAndSaveConfigAsync(deployRequest, flyioAccessToken);
 
         if (hasBeenDeployed)
         {
-            await RedeployFlyAppAsync(deployRequest, flyioAccessToken);
+            if (!await RedeployFlyAppAsync(deployRequest, flyioAccessToken, newTag))
+            {
+                return new DeployResponse(false, "Failed to redeploy Fly.io app");
+            }
         }
         else
         {
-            await LaunchFlyAppAsync(deployRequest, flyioAccessToken);
+            if (!await LaunchFlyAppAsync(deployRequest, flyioAccessToken, newTag))
+            {
+                return new DeployResponse(false, "Failed to launch Fly.io app");
+            }
         }
 
         return new DeployResponse(true, "Deployed to Fly.io");
     }
 
-    private async Task LaunchFlyAppAsync(DeployRequest deployRequest, string accessToken)
+    private async Task<bool> AuthenticateFlyioWithDockerAsync()
+    {
+        var response = await processService.RunProcessAsync("fly auth docker");
+
+        if (!response.Success)
+        {
+            Log.ForContext("output", response.Output)
+               .Error("Failed to authenticate Fly.io with Docker");
+            return false;
+        }
+
+        Log.Information("Fly.io authenticated with Docker");
+        return true;
+    }
+
+    private async Task<string?> TagAndPushImageToFlyioAsync(string appName, string imageTag)
+    {
+        var newTag = $"registry.fly.io/{appName}";
+
+        var response = await processService.RunProcessAsync($@"
+            docker tag {imageTag} {newTag}");
+
+        if (!response.Success)
+        {
+            Log.ForContext("output", response.Output)
+               .Error("Failed to tag image to Fly.io");
+            return null;
+        }
+
+        Log.Information("Image tagged to Fly.io");
+
+        response = await processService.RunProcessAsync($@"
+            docker push {newTag}");
+
+        if (!response.Success)
+        {
+            Log.ForContext("output", response.Output)
+               .Error("Failed to push image to Fly.io");
+            return null;
+        }   
+
+        return newTag;
+    }
+
+    private async Task<bool> LaunchFlyAppAsync(DeployRequest deployRequest, string accessToken, string newTag)
     {
         Log.Information("Launching Fly.io app");
         await CreateDefaultJsonFileAsync(deployRequest.AppName, deployRequest.RegistryPath, deployRequest.Platform);
 
         var response = await processService.RunProcessAsync($@"
         fly launch 
-            --app {deployRequest.AppName} 
             --config {FlyioJsonFileName}
             --json 
-            --image {deployRequest.RegistryPath}
+            --image {newTag}
+            --name {deployRequest.AppName}
+            --region fra
+            --vm-size shared-cpu-1x
+            --org compozerr
             --access-token {accessToken} 
             --yes");
 
@@ -56,13 +121,15 @@ public class FlyioHostingProvider(
         {
             Log.ForContext("output", response.Output)
                .Error("Failed to launch Fly.io app");
-            return;
+            return false;
         }
 
         Log.Information("Fly.io app launched");
+
+        return true;
     }
 
-    private async Task RedeployFlyAppAsync(DeployRequest deployRequest, string accessToken)
+    private async Task<bool> RedeployFlyAppAsync(DeployRequest deployRequest, string accessToken, string newTag)
     {
         Log.Information("Redeploying Fly.io app");
         var response = await processService.RunProcessAsync($@"
@@ -70,7 +137,7 @@ public class FlyioHostingProvider(
                 --app {deployRequest.AppName} 
                 --config {FlyioJsonFileName}
                 --json
-                --image {deployRequest.RegistryPath}
+                --image {newTag}
                 --access-token {accessToken} 
                 --yes");
 
@@ -78,10 +145,12 @@ public class FlyioHostingProvider(
         {
             Log.ForContext("output", response.Output)
                .Error("Failed to redeploy Fly.io app");
-            return;
+            return false;
         }
 
         Log.Information("Fly.io app redeployed");
+
+        return true;
     }
 
     private async Task<bool> HasBeenDeployedAndSaveConfigAsync(DeployRequest deployRequest, string accessToken)
