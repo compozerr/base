50f541daf8507f318b35de59fe4c5f82ed6b4ed4
Milo <tomilobas@gmail.com>
2024-11-23 13:33:56 +0100

    Added Humio sink

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -1,13 +1,25 @@
 using Core.Feature;
 using Serilog;
+using Serilog.Sinks.Humio;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration().WriteTo.Console()
-                                              .CreateLogger();
-
 builder.ConfigureFeatures();
 builder.Services.AddFeatures();
+
+Log.Logger = new LoggerConfiguration().WriteTo.Console().WriteTo
+                                              .HumioSink(new HumioSinkConfiguration
+                                              {
+                                                  IngestToken = builder.Configuration["HUMIOINGESTTOKEN"],
+                                                  Tags = new Dictionary<string, string>
+                                                  {
+                                                      {"system", "compozerr"},
+                                                      {"platform", "web"},
+                                                      {"environment", builder.Environment.EnvironmentName}
+                                                  },
+                                                  Url = "https://cloud.community.humio.com",
+                                              })
+                                              .CreateLogger();
 
 var app = builder.Build();
 
