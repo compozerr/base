6500bf1b79239f1e09ca14658f951cc61dd88b4b
Milo <tomilobas@gmail.com>
2024-11-29 07:59:02 +0100

    Formatting

--- backend/Core/MediatR/AddMediatorServiceCollection.cs
+++ backend/Core/MediatR/AddMediatorServiceCollection.cs
@@ -5,9 +5,9 @@ namespace Core.MediatR;
 public static class AddMediatorServiceCollection
 {
     public static IServiceCollection UseMediatR(this IServiceCollection services)
-    => services.AddMediatR(c =>
-                               {
-                                   c.Lifetime = ServiceLifetime.Scoped;
-                                   c.RegisterServicesFromAssemblyContaining(typeof(ServiceCollectionExtensions));
-                               });
+        => services.AddMediatR(c =>
+                                {
+                                    c.Lifetime = ServiceLifetime.Scoped;
+                                    c.RegisterServicesFromAssemblyContaining(typeof(ServiceCollectionExtensions));
+                                });
 }
