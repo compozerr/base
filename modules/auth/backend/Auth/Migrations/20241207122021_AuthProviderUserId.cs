a735e58492e3d17ce3ddf24484e6676da950abeb
Milo <tomilobas@gmail.com>
2024-12-07 13:20:43 +0100

    Added Migration

---
+++ modules/auth/backend/Auth/Migrations/20241207122021_AuthProviderUserId.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthProviderUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthProviderUserId",
+                schema: "auth",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthProviderUserId",
+                schema: "auth",
+                table: "Users");
+        }
+    }
+}
