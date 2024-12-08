83824f8a0f51c1011e264d0052fb72525dd08bd7
Milo <tomilobas@gmail.com>
2024-12-08 15:06:46 +0100

    Using kiota instead

--- frontend/src/generated/index.ts
+++
@@ -1,5 +0,0 @@
-export { AuthApi } from "./Auth";
-export { DockerApi } from "./Docker";
-export { ExampleApi } from "./Example";
-export { HomeApi } from "./Home";
-export { UsersApi } from "./Users";
