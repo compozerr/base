dff8e52b78ee1ff0ccce681aae44ef96394af2b1
Milo <tomilobas@gmail.com>
2024-12-08 18:01:56 +0100

    Better install script

--- scripts/install.ts
+++ scripts/install.ts
@@ -1,50 +1,97 @@
 import { exec } from "child_process";
 
-console.log("Installing dependencies...");
+//#region Helper functions
+const executeCommandAsync = (command: string) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (stderr || error) {
+                reject(stderr || error);
+            }
+            else {
+                resolve(stdout);
+            }
+        });
+    });
+}
 
-let denoInstalled = true;
+const hasDotnetToolAsync = async (command: string) => {
+    try {
+        await executeCommandAsync(`dotnet tool list -g | grep ${command} || echo "${command} not installed" >&2`)
+        return true;
+    }
+    catch {
+        return false;
+    }
+}
 
-//Check if deno is installed
-exec("deno --version", (error, stdout, stderr) => {
-    if (error) {
+const installDotnetToolIfNotExistsAsync = async (command: string) => {
+    if (!await hasDotnetToolAsync(command)) {
+        console.log(`${command} is not installed, installing...`);
+        try {
+
+            await executeCommandAsync(`dotnet tool install -g ${command}`);
+            console.log(`${command} installed`);
+        }
+        catch (error) {
+            console.error(`Error installing ${command}`);
+            throw error;
+        }
+    }
+    else {
+        console.log(`${command} is already installed`);
+    }
+}
+//#endregion
+
+//#region Install deno if not exists
+const installDenoIfNotExistsAsync = async () => {
+    let denoInstalled = true;
+
+    try {
+        //Check if deno is installed
+        await executeCommandAsync("deno --version");
+    }
+    catch {
         denoInstalled = false;
-        return;
     }
-});
 
-if (!denoInstalled) {
-    //if windows
-    if (process.platform === "win32") {
+    if (!denoInstalled) {
         console.log("Deno is not installed, installing...");
-        exec("iwr https://deno.land/x/install/install.ps1 -useb | iex");
-    } else if (process.platform === "linux" || process.platform === "darwin") {
-        console.log("Deno is not installed, installing...");
-        exec("curl -fsSL https://deno.land/x/install/install.sh | sh");
-    } else {
-        console.error("Unsupported platform");
+        if (process.platform === "win32") {
+            await executeCommandAsync("iwr https://deno.land/x/install/install.ps1 -useb | iex");
+        } else if (process.platform === "linux" || process.platform === "darwin") {
+            await executeCommandAsync("curl -fsSL https://deno.land/x/install/install.sh | sh");
+        } else {
+            console.error("Unsupported platform, cannot install deno");
+            throw new Error("Unsupported platform");
+        }
     }
 }
+//#endregion
 
-let nbgvInstalled = true;
-
-//Check if nbgv is installed
-exec("dotnet tool list -g | findstr nbgv", (error, stdout, stderr) => {
-    if (error) {
-        nbgvInstalled = false;
-        return;
+//#region Install frontned dependencies
+const installFrontendDependenciesAsync = async () => {
+    try {
+        await executeCommandAsync("npm install");
+        console.log("Frontend dependencies installed");
     }
-});
-
-if (!nbgvInstalled) {
-    console.log("nbgv is not installed, installing...");
-    exec("dotnet tool install nbgv");
-}
-
-exec("npm install", (error, stdout, stderr) => {
-    if (error) {
+    catch (error) {
         console.error("Error installing frontend dependencies");
-        return;
+        throw error;
     }
-});
+}
+//#endregion
 
-console.log("Dependencies installed!");
+(async function main() {
+    console.log("Installing all dependencies...");
+
+    await installDenoIfNotExistsAsync();
+
+    await installDotnetToolIfNotExistsAsync("nbgv");
+    await installDotnetToolIfNotExistsAsync("microsoft.openapi.kiota");
+
+    await installFrontendDependenciesAsync();
+
+    console.log("All dependencies installed!");
+})();
+
