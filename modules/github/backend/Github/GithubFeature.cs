587dc309101a367d2746ddc8e2fced5e295f2724
Milo <tomilobas@gmail.com>
2024-12-14 11:10:59 +0100

    Migrate db if in development for githubdbcontext

--- modules/github/backend/Github/GithubFeature.cs
+++ modules/github/backend/Github/GithubFeature.cs
@@ -1,17 +1,32 @@
 using Core.Extensions;
 using Core.Feature;
+using Github.Data;
 using Github.Options;
 using Github.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Github;
 
-public class ExampleFeature : IFeature
+public class GithubFeature : IFeature
 {
     void IFeature.ConfigureServices(IServiceCollection services)
     {
         services.AddRequiredConfigurationOptions<GithubAppOptions>("Github:GithubApp");
         services.AddSingleton<IStateService, StateService>();
         services.AddDataProtection();
+    }
+
+    void IFeature.ConfigureApp(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<GithubDbContext>();
+
+            context.Database.Migrate();
+        }
     }
 }
