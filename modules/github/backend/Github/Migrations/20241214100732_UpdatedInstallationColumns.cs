7e61d0c3c87701b3080d601ea48bdec3c7a30cf5
Milo <tomilobas@gmail.com>
2024-12-14 11:07:54 +0100

    Update Installation columns

---
+++ modules/github/backend/Github/Migrations/20241214100732_UpdatedInstallationColumns.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Github.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedInstallationColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiresAtUtc",
+                schema: "github",
+                table: "Installations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Scope",
+                schema: "github",
+                table: "Installations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Scope",
+                schema: "github",
+                table: "Installations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiresAtUtc",
+                schema: "github",
+                table: "Installations",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
