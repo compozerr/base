9061ed0de535065a0b156db2968dbbb67667ece8
Milo <tomilobas@gmail.com>
2024-12-15 18:46:56 +0100

    Migration for dropping column ExpiresAtUtc

---
+++ modules/auth/backend/Auth/Migrations/20241215174640_RemovedExpiredAtUtc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedExpiredAtUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiresAtUtc",
+                schema: "auth",
+                table: "UserLogins");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiresAtUtc",
+                schema: "auth",
+                table: "UserLogins",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
