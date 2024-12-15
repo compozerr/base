ced98672120588614dafd7a96ab55779d669f5b0
Milo <tomilobas@gmail.com>
2024-12-15 18:48:11 +0100

    Renamed

--- modules/github/backend/Github/Endpoints/Installation/GetInstallAppUrlRoute.cs
+++ modules/github/backend/Github/Endpoints/Installation/GetInstallAppUrlRoute.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Builder;
 
 namespace Github.Endpoints.Installation;
 
-public static class GetInstallationAuthorizeUrlRoute
+public static class GetInstallAppUrlRoute
 {
     public const string Route = "get-install-app-url";
     public static RouteHandlerBuilder AddGetInstallAppUrlRoute(this IEndpointRouteBuilder app)
