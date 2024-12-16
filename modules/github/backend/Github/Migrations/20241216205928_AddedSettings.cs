6d0934afacc67e05fd174bda5217080340b6995f
Milo <tomilobas@gmail.com>
2024-12-16 21:59:49 +0100

    Added migration for github user settings

---
+++ modules/github/backend/Github/Migrations/20241216205928_AddedSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Github.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "github");
+
+            migrationBuilder.CreateTable(
+                name: "GithubUserSettings",
+                schema: "github",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SelectedOrganization = table.Column<string>(type: "text", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GithubUserSettings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GithubUserSettings",
+                schema: "github");
+        }
+    }
+}
