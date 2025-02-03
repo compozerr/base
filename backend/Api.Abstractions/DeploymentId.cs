cbebccd6efda10c57ca0063ed1444edea40fb216
Milo <tomilobas@gmail.com>
2025-02-03 20:41:58 +0100

    Simplified using primary contstructor

--- backend/Api.Abstractions/DeploymentId.cs
+++ backend/Api.Abstractions/DeploymentId.cs
@@ -1,11 +1,7 @@
 namespace Api.Abstractions;
 
-public sealed record DeploymentId : IdBase<DeploymentId>, IId<DeploymentId>
+public sealed record DeploymentId(Guid Value) : IdBase<DeploymentId>(Value), IId<DeploymentId>
 {
-    public DeploymentId(Guid value) : base(value)
-    {
-    }
-
     public static DeploymentId Create(Guid value)
         => new(value);
 }
