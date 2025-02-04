c3b4f441a05353ee4138e82e969cc0e417c693f9
Milo <tomilobas@gmail.com>
2025-02-04 21:04:31 +0100

    Added comments to get values

--- backend/Api.Data/Server.cs
+++ backend/Api.Data/Server.cs
@@ -5,9 +5,9 @@ public class Server : BaseEntityWithId<ServerId>
     public LocationId? LocationId { get; set; }
     public SecretId SecretId { get; set; } = null!;
     public string MachineId { get; set; } = string.Empty; // cat /etc/machine-id
-    public string Ram { get; set; } = string.Empty;
-    public string VCpu { get; set; } = string.Empty;
-    public string Ip { get; set; } = string.Empty;
+    public string Ram { get; set; } = string.Empty; // cat /proc/meminfo | grep "MemAvailable:" and get ram and convert to gb
+    public string VCpu { get; set; } = string.Empty; // lscpu | grep -E "Core\(s\) per socket:|Thread\(s\) per core:" and get core count and get thread per core times core count e.g. 8c/16t
+    public string Ip { get; set; } = string.Empty; // curl api.ipify.org
 
     public virtual Location? Location { get; set; }
     public virtual Secret? Secret { get; set; }
