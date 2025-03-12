0d384694d44dfdc52499e4964d86cf1ad4df9f47
Milo <tomilobas@gmail.com>
2025-03-12 20:30:29 +0100

    Added locationid and serverid per project

---
+++ backend/Api.Data/Migrations/20250312193043_LocationId and ServerId for project.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LocationIdandServerIdforproject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LocationId",
+                schema: "api",
+                table: "Projects",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ServerId",
+                schema: "api",
+                table: "Projects",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                schema: "api",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ServerId",
+                schema: "api",
+                table: "Projects");
+        }
+    }
+}
--- backend/Api.Data/Project.cs
+++ backend/Api.Data/Project.cs
@@ -7,6 +7,8 @@ public class Project : BaseEntityWithId<ProjectId>
     public required string Name { get; set; }
     public required Uri RepoUri { get; set; }
     public required UserId UserId { get; set; }
+    public required LocationId LocationId { get; set; }
+    public ServerId? ServerId { get; set; }
 
     public virtual ICollection<ProjectEnvironment>? ProjectEnvironments { get; set; }
 }
