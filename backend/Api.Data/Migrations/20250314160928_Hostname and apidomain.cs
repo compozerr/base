8544e0f2b6870d6df8a57231bc533ad618845817
Milo R. Jørgensen <mrj@holion.dk>
2025-03-14 17:09:48 +0100

    Added hostname and ApiDomain for Server model

---
+++ backend/Api.Data/Migrations/20250314160928_Hostname and apidomain.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Hostnameandapidomain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiDomain",
+                schema: "api",
+                table: "Servers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HostName",
+                schema: "api",
+                table: "Servers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiDomain",
+                schema: "api",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "HostName",
+                schema: "api",
+                table: "Servers");
+        }
+    }
+}
--- backend/Api.Data/Server.cs
+++ backend/Api.Data/Server.cs
@@ -8,6 +8,8 @@ public class Server : BaseEntityWithId<ServerId>
     public string Ram { get; set; } = string.Empty; // cat /proc/meminfo | grep "MemAvailable:" and get ram and convert to gb
     public string VCpu { get; set; } = string.Empty; // lscpu | grep -E "Core\(s\) per socket:|Thread\(s\) per core:" and get core count and get thread per core times core count e.g. 8c/16t
     public string Ip { get; set; } = string.Empty; // curl api.ipify.org
+    public string HostName { get; set; } = string.Empty; // $(hostname)
+    public string ApiDomain { get; set; } = string.Empty;
 
     public virtual Location? Location { get; set; }
     public virtual Secret? Secret { get; set; }
