28ccaf92678a647f0cd83d04feb238655dc753df
Milo <tomilobas@gmail.com>
2025-03-14 20:28:04 +0100

    Updated install script

--- frontend/public/install.sh
+++ frontend/public/install.sh
@@ -15,18 +15,14 @@ fi
 LATEST_VERSION=$(curl -s https://storage.googleapis.com/compozerr.firebasestorage.app/versions.json?cachebust=$(date +%s) | jq -r 'max_by(split(".") | map(tonumber))' | tr -d '"')
 
 printf "Downloading compozerr v${LATEST_VERSION}...\n"
-# Download the latest version
-curl -s https://storage.googleapis.com/compozerr.firebasestorage.app/${LATEST_VERSION} -o compozerr
 
-# Make the binary executable
-chmod +x compozerr
-
-# Move the binary to the PATH
+# Download directly to /usr/local/bin with sudo if needed
 if [ -w "/usr/local/bin" ]; then
-    mv compozerr /usr/local/bin/compozerr
+    curl -s https://storage.googleapis.com/compozerr.firebasestorage.app/${LATEST_VERSION} -o /usr/local/bin/compozerr
+    chmod +x /usr/local/bin/compozerr
 else
-    echo "Need sudo permissions to install to add to PATH"
-    sudo mv compozerr /usr/local/bin/compozerr
+    sudo curl -s https://storage.googleapis.com/compozerr.firebasestorage.app/${LATEST_VERSION} -o /usr/local/bin/compozerr
+    sudo chmod +x /usr/local/bin/compozerr
 fi
 
 echo "compozerr installed successfully"
