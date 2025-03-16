858b765b3c293fbb8d0a427e312cbf2275f3bae6
Milo <tomilobas@gmail.com>
2025-03-16 20:26:28 +0100

    Upgraded Serilog

--- backend/Core/CoreFeature.cs
+++ backend/Core/CoreFeature.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 
 namespace Core;
 
