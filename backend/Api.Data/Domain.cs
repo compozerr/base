1e381932211679416cd08093662ca3ade5cd6888
Milo <tomilobas@gmail.com>
2025-03-16 18:05:32 +0100

    Added domain port

--- backend/Api.Data/Domain.cs
+++ backend/Api.Data/Domain.cs
@@ -6,6 +6,8 @@ public class Domain : BaseEntityWithId<DomainId>
     public required ServerId ServerId { get; set; }
 
     public required DomainType Type { get; set; }
+    
+    public required string Port { get; set; }
 
     public virtual Server? Server { get; set; }
 }
