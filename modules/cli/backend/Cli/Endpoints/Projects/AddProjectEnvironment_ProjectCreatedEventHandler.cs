3e8f95b0545588f1fca543815cfac5b026d3d5a3
Milo <tomilobas@gmail.com>
2025-03-27 21:22:21 +0100

    Add project environment when created

---
+++ modules/cli/backend/Cli/Endpoints/Projects/AddProjectEnvironment_ProjectCreatedEventHandler.cs
@@ -0,0 +1,20 @@
+using Api.Data;
+using Core.Abstractions;
+
+namespace Cli.Endpoints.Projects;
+
+public sealed class AddProjectEnvironment_ProjectCreatedEventHandler : IDomainEventHandler<ProjectCreatedEvent>
+{
+    public Task Handle(ProjectCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        var defaultEnvironment = new ProjectEnvironment
+        {
+            ProjectId = notification.Entity.Id,
+            Branches = ["main"]
+        };
+
+        notification.Entity.ProjectEnvironments = [defaultEnvironment];
+
+        return Task.CompletedTask;
+    }
+}
