29f91524f86ef67362c8ea4ec9b60a587facd332
Milo <tomilobas@gmail.com>
2025-03-27 18:13:51 +0100

    Do not register mediatr services more than once

--- backend/Core/MediatR/RegisterMediatRServicesFeatureConfigureCallback.cs
+++ backend/Core/MediatR/RegisterMediatRServicesFeatureConfigureCallback.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Feature;
 using MediatR.Registration;
 using Microsoft.AspNetCore.Builder;
@@ -7,9 +8,15 @@ namespace Core.MediatR;
 
 public class RegisterMediatrServicesFeatureConfigureCallback : IFeatureConfigureCallback
 {
+    private static readonly HashSet<Assembly> assemblies = [];
+
     void IFeatureConfigureCallback.Configure(Type type, WebApplicationBuilder builder)
-        => ServiceRegistrar.AddMediatRClasses(
+    {
+        if (!assemblies.Add(type.Assembly)) return;
+
+        ServiceRegistrar.AddMediatRClasses(
             builder.Services,
             new MediatRServiceConfiguration()
                 .RegisterServicesFromAssemblyContaining(type));
+    }
 }
