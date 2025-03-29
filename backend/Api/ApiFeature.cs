76434809894e6c39d3e0bb119bd46e2dac323383
Milo <tomilobas@gmail.com>
2025-03-29 17:02:10 +0100

    Removed all else in program.cs

--- backend/Api/ApiFeature.cs
+++ backend/Api/ApiFeature.cs
@@ -3,17 +3,40 @@ using Api.Options;
 using Api.Services;
 using Core.Extensions;
 using Core.Feature;
+using Serilog.Events;
+using Serilog.Sinks.Humio;
 
 namespace Api;
 
 public class ApiFeature : IFeature
 {
-    void IFeature.ConfigureServices(IServiceCollection services)
+    void IFeature.ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IEncryptionService, EncryptionService>();
         services.AddScoped<IServerService, ServerService>();
         services.AddScoped<IDefaultEnvironmentVariablesAppender, DefaultEnvironmentVariablesAppender>();
 
         services.AddRequiredConfigurationOptions<EncryptionOptions>("Encryption");
+    }
+
+    void IFeature.ConfigureBuilder(WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.HumioSink(new HumioSinkConfiguration
+            {
+                IngestToken = builder.Configuration["HUMIOINGESTTOKEN"],
+                Tags = new Dictionary<string, string>
+                {
+                    {"system", "compozerr"},
+                    {"platform", "web"},
+                    {"environment", builder.Environment.EnvironmentName}
+                },
+                Url = "https://cloud.community.humio.com",
+            })
+            .CreateLogger();
     }
 }
