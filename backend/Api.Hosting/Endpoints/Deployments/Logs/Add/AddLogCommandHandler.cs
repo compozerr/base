e543b5a83e96f81cbfca9f100c3b7f2ddd54c997
Milo <tomilobas@gmail.com>
2025-04-05 18:02:08 +0200

    Logs

--- backend/Api.Hosting/Endpoints/Deployments/Logs/Add/AddLogCommandHandler.cs
+++ backend/Api.Hosting/Endpoints/Deployments/Logs/Add/AddLogCommandHandler.cs
@@ -1,5 +1,6 @@
 using Api.Abstractions.Helpers;
 using Core.MediatR;
+using Serilog;
 using Storage;
 
 namespace Api.Hosting.Endpoints.Deployments.Logs.Add;
@@ -9,14 +10,19 @@ public sealed class AddLogCommandHandler(
 {
     public async Task<AddLogResponse> Handle(AddLogCommand command, CancellationToken cancellationToken = default)
     {
+        Log.ForContext("deploymentId", command.DeploymentId).Information("Adding log");
         var currentLog = await storageService.DownloadAsync(LogHelpers.GetLogFileName(command.DeploymentId), cancellationToken) ?? new MemoryStream();
+        Log.ForContext("currentLog", currentLog.Length).Information("Got current log");
 
         var writer = new StreamWriter(currentLog);
         writer.WriteLine(LogHelpers.GetLogEntry(command.Log, command.Level));
         writer.Flush();
         currentLog.Position = 0;
+        Log.ForContext("currentLog", currentLog.Length).Information("Uploading log");
 
         await storageService.UploadAsync(LogHelpers.GetLogFileName(command.DeploymentId), currentLog, cancellationToken);
+        
+        Log.ForContext("deploymentId", command.DeploymentId).Information("Uploaded log");
 
         return new AddLogResponse();
     }
