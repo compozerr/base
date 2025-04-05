1c8e8e33b159166202d5574a761368edb7b3004c
Milo <tomilobas@gmail.com>
2025-04-05 18:11:05 +0200

    REMOVE ME

--- backend/Api.Hosting/Endpoints/Deployments/Logs/Add/AddLogCommandHandler.cs
+++ backend/Api.Hosting/Endpoints/Deployments/Logs/Add/AddLogCommandHandler.cs
@@ -6,21 +6,22 @@ using Storage;
 namespace Api.Hosting.Endpoints.Deployments.Logs.Add;
 
 public sealed class AddLogCommandHandler(
-    IStorageService storageService) : ICommandHandler<AddLogCommand, AddLogResponse>
+    // IStorageService storageService
+    ) : ICommandHandler<AddLogCommand, AddLogResponse>
 {
     public async Task<AddLogResponse> Handle(AddLogCommand command, CancellationToken cancellationToken = default)
     {
         Log.ForContext("deploymentId", command.DeploymentId).Information("Adding log");
-        var currentLog = await storageService.DownloadAsync(LogHelpers.GetLogFileName(command.DeploymentId), cancellationToken) ?? new MemoryStream();
-        Log.ForContext("currentLog", currentLog.Length).Information("Got current log");
+        // var currentLog = await storageService.DownloadAsync(LogHelpers.GetLogFileName(command.DeploymentId), cancellationToken) ?? new MemoryStream();
+        // Log.ForContext("currentLog", currentLog.Length).Information("Got current log");
 
-        var writer = new StreamWriter(currentLog);
-        writer.WriteLine(LogHelpers.GetLogEntry(command.Log, command.Level));
-        writer.Flush();
-        currentLog.Position = 0;
-        Log.ForContext("currentLog", currentLog.Length).Information("Uploading log");
+        // var writer = new StreamWriter(currentLog);
+        // writer.WriteLine(LogHelpers.GetLogEntry(command.Log, command.Level));
+        // writer.Flush();
+        // currentLog.Position = 0;
+        // Log.ForContext("currentLog", currentLog.Length).Information("Uploading log");
 
-        await storageService.UploadAsync(LogHelpers.GetLogFileName(command.DeploymentId), currentLog, cancellationToken);
+        // await storageService.UploadAsync(LogHelpers.GetLogFileName(command.DeploymentId), currentLog, cancellationToken);
         
         Log.ForContext("deploymentId", command.DeploymentId).Information("Uploaded log");
 
