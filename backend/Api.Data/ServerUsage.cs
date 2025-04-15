f1d6c8d87e8a88eec3fcb2b09f3c728702bb7e12
Milo <tomilobas@gmail.com>
2025-04-15 18:41:57 +0200

    Added Json properties for the server usage

--- backend/Api.Data/ServerUsage.cs
+++ backend/Api.Data/ServerUsage.cs
@@ -1,8 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace Api.Data;
 
-public sealed record ServerUsage 
+public sealed record ServerUsage
 {
+
+    [JsonPropertyName("ramPercentage")]
     public decimal AvgRamPercentage { get; set; } = 0;
+    [JsonPropertyName("cpuPercentage")]
     public decimal AvgCpuPercentage { get; set; } = 0;
+    [JsonPropertyName("diskPercentage")]
     public decimal AvgDiskPercentage { get; set; } = 0;
 }
