9fcd8120d41d98966afca24f4a3847636b12f784
Milo <tomilobas@gmail.com>
2025-04-27 17:37:46 +0200

    Delete project command handler implementation

--- backend/Api/Endpoints/Projects/Project/Delete/DeleteProjectCommandHandler.cs
+++ backend/Api/Endpoints/Projects/Project/Delete/DeleteProjectCommandHandler.cs
@@ -1,14 +1,25 @@
 using Api.Data.Repositories;
+using Api.Hosting.Services;
 using Core.MediatR;
 
 namespace Api.Endpoints.Projects.Project.Delete;
 
 public sealed class DeleteProjectCommandHandler(
-    IProjectRepository projectRepository) : ICommandHandler<DeleteProjectCommand, DeleteProjectResponse>
+    IProjectRepository projectRepository,
+    IHostingApiFactory hostingApiFactory) : ICommandHandler<DeleteProjectCommand, DeleteProjectResponse>
 {
     public async Task<DeleteProjectResponse> Handle(DeleteProjectCommand command, CancellationToken cancellationToken = default)
     {
-        
-        return new DeleteProjectResponse();
+        var project = await projectRepository.GetByIdAsync(command.ProjectId, cancellationToken) ?? throw new InvalidOperationException("Should not be able to be here if null");
+
+        if (project.ServerId is { } serverId)
+        {
+            var hostingApi = await hostingApiFactory.GetHostingApiAsync(serverId);
+            await hostingApi.DeleteProjectAsync(project.Id);
+        }
+
+        await projectRepository.DeleteAsync(project.Id, cancellationToken);
+
+        return new();
     }
 }
