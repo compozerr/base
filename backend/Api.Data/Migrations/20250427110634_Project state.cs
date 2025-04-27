5a6642a728409f37e1b0562fff8521ecc42c7662
Milo <tomilobas@gmail.com>
2025-04-27 13:06:49 +0200

    Added project state migration

---
+++ backend/Api.Data/Migrations/20250427110634_Project state.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Projectstate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                schema: "api",
+                table: "Projects",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                schema: "api",
+                table: "Projects");
+        }
+    }
+}
