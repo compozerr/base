8add2a3ea8d6b2e4958777d274f10dc5e4f680c5
Milo <tomilobas@gmail.com>
2025-06-15 21:04:01 +0200

    Added migrations for stripe customer

---
+++ modules/stripe/backend/Stripe/Migrations/20250615190337_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stripe.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "stripe");
+
+            migrationBuilder.CreateTable(
+                name: "StripeCustomers",
+                schema: "stripe",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    InternalId = table.Column<string>(type: "text", nullable: false),
+                    StripeCustomerId = table.Column<string>(type: "text", nullable: true),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StripeCustomers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StripeCustomers",
+                schema: "stripe");
+        }
+    }
+}
