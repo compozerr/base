7458d509ebc153224bc7358443505c784e0105ea
Milo <tomilobas@gmail.com>
2025-06-15 13:17:46 +0200

    Removed unused using

--- modules/auth/backend/Auth/AuthFeature.cs
+++ modules/auth/backend/Auth/AuthFeature.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Auth;
