c26b5268ccadd1e83f9ea39b14741644890e8c03
Milo <tomilobas@gmail.com>
2025-08-20 16:51:47 +0200

    Get internalId by stripes customer id

--- modules/stripe/backend/Stripe/Data/Repositories/StripeCustomerRepository.cs
+++ modules/stripe/backend/Stripe/Data/Repositories/StripeCustomerRepository.cs
@@ -7,18 +7,26 @@ namespace Stripe.Data.Repositories;
 
 public interface IStripeCustomerRepository : IGenericRepository<StripeCustomer, StripeCustomerId, StripeDbContext>
 {
-    Task<string?> GetStripeCustomerIdByInternalId(string internalId);
+    Task<string?> GetStripeCustomerIdByInternalIdAsync(string internalId, CancellationToken cancellationToken = default);
+    Task<string?> GetInternalIdByStripeCustomerIdAsync(string customerId, CancellationToken cancellationToken = default);
     Task SetStripeCustomerIdAsync(string internalId, string customerId);
 }
 
 public sealed class StripeCustomerRepository(
     StripeDbContext context) : GenericRepository<StripeCustomer, StripeCustomerId, StripeDbContext>(context), IStripeCustomerRepository
 {
-    public Task<string?> GetStripeCustomerIdByInternalId(string internalId)
+    public Task<string?> GetInternalIdByStripeCustomerIdAsync(string customerId, CancellationToken cancellationToken = default)
+    {
+        return Query().Where(x => x.StripeCustomerId == customerId)
+            .Select(x => x.InternalId)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public Task<string?> GetStripeCustomerIdByInternalIdAsync(string internalId, CancellationToken cancellationToken = default)
     {
         return Query().Where(x => x.InternalId == internalId)
                       .Select(x => x.StripeCustomerId)
-                      .FirstOrDefaultAsync();
+                      .FirstOrDefaultAsync(cancellationToken);
     }
 
     public Task SetStripeCustomerIdAsync(string internalId, string customerId)
