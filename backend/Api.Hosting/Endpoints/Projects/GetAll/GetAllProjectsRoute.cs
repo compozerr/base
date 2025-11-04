6e87df025e4f8c81cdc654e008cfd46d054d6c68
Milo <tomilobas@gmail.com>
2025-11-04 18:58:55 +0100

    Fixed get all projects route

--- backend/Api.Hosting/Endpoints/Projects/GetAll/GetAllProjectsRoute.cs
+++ backend/Api.Hosting/Endpoints/Projects/GetAll/GetAllProjectsRoute.cs
@@ -1,12 +1,14 @@
+using Api.Abstractions;
 using Api.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
 namespace Api.Hosting.Endpoints.Projects.GetAll;
 
-public sealed record ProjectDto(Guid ProjectId);
+public sealed record ProjectDto(ProjectId ProjectId);
 
 public static class GetAllProjectsRoute
 {
@@ -18,12 +20,12 @@ public static class GetAllProjectsRoute
     }
 
     public static async Task<Ok<List<ProjectDto>>> ExecuteAsync(
-        IProjectRepository projectRepository)
+        [FromServices] IProjectRepository projectRepository)
     {
         var projects = await projectRepository.GetAllAsync();
 
         var projectDtos = projects
-            .Select(p => new ProjectDto(p.Id.Value))
+            .Select(p => new ProjectDto(p.Id))
             .ToList();
 
         return TypedResults.Ok(projectDtos);
