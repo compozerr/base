93558fdf55882102d0df6ad6fd4a99e7ec89b504
Milo <tomilobas@gmail.com>
2025-11-11 19:58:36 +0100

    IDomainEvent instead of INotification

--- backend/Core.Abstractions/IDomainEventWithTiming.cs
+++ backend/Core.Abstractions/IDomainEventWithTiming.cs
@@ -1,12 +1,10 @@
-using MediatR;
-
 namespace Core.Abstractions;
 
 /// <summary>
 /// Internal marker interface for domain events wrapped with timing information.
 /// This is used by the infrastructure to dispatch events at the correct timing.
 /// </summary>
-public interface IDomainEventWithTiming : INotification
+public interface IDomainEventWithTiming : IDomainEvent
 {
     DomainEventTiming Timing { get; }
 }
