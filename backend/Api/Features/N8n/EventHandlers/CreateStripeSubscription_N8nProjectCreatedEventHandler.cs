d97ac7faccdad9f8f371c45ccf2ca9a6ca23abb3
Milo <tomilobas@gmail.com>
2025-11-22 21:27:58 +0100

    Added create stripe subscription for n8n project

---
+++ backend/Api/Features/N8n/EventHandlers/CreateStripeSubscription_N8nProjectCreatedEventHandler.cs
@@ -0,0 +1,29 @@
+using Api.Abstractions;
+using Api.Features.N8n.Events;
+using Core.Abstractions;
+using Github.Services;
+using MediatR;
+using Stripe.Endpoints.Subscriptions.UpsertSubscription;
+
+namespace Api.Features.N8n.EventHandlers;
+
+public sealed class CreateStripeSubscription_N8nProjectCreatedEventHandler(
+    ISender sender)
+    : EntityDomainEventHandlerBase<N8nProjectCreatedEvent>
+{
+    protected override async Task HandleAfterSaveAsync(
+        N8nProjectCreatedEvent domainEvent,
+        CancellationToken cancellationToken)
+    {
+        return;
+        
+        var command = new UpsertSubscriptionCommand(
+             domainEvent.Entity.Id,
+             domainEvent.Entity.ServerTierId,
+             "BLACKFRIDAY2025");
+
+        await sender.Send(
+            command,
+            cancellationToken);
+    }
+}
